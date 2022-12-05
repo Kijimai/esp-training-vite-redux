12f3ef36c0353eb452aa8758fa381b96ebfcd3d6
John Deen Bucog <JDBucog@yahoo.com>
2022-12-04 21:56:03 -0800

    init PurchaseCard component

--- src/components/purchase/PurchaseCard.jsx
+++ src/components/purchase/PurchaseCard.jsx
@@ -1,7 +1,26 @@
 import styled from "styled-components"
+import { formatPrice } from "../../utils/helper"
 
-const PurchaseCard = ({ color }) => {
-  return <div>PurchaseCard</div>
+const PurchaseCard = ({ title, cost, offers, bannerColor, userType }) => {
+  return (
+    <CardWrapper>
+      <header>
+        <h3>
+          {title} / {formatPrice(cost)}
+        </h3>
+      </header>
+      <ul>
+        {offers.map((offer, idx) => {
+          return <li key={idx}>{offer}</li>
+        })}
+      </ul>
+      <footer>
+        <button>Purchase</button>
+      </footer>
+    </CardWrapper>
+  )
 }
+
+const CardWrapper = styled.article``
 
 export default PurchaseCard
