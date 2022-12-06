bd58002877d7bf157d329e94c10dd67900d6ddf5
John Deen Bucog <JDBucog@yahoo.com>
2022-12-06 15:40:55 -0800

    add Toastify, ToastContainer into App.js, wrap dashboard into PrivateRoute component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import SharedLayout from "./components/SharedLayout"
-import { Main, Payment, Login, Purchase, Services, Team } from "./routes/index"
+import {
+  Main,
+  Payment,
+  Purchase,
+  Services,
+  Team,
+  Dashboard,
+  PrivateRoute,
+} from "./routes/index"
 import Sidebar from "./components/navigator/Sidebar"
 import { useSelector } from "react-redux"
 import AuthWrapper from "./routes/AuthWrapper"
+
+// Toast popup dependencies
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+const toastOptions = {
+  position: "top-center",
+  autoClose: 5000,
+  hideProgressBar: false,
+  newestOnTop: false,
+  closeOnClick: true,
+  rtl: false,
+  pauseOnFocusLoss: true,
+  draggable: false,
+  pauseOnHover: true,
+  theme: "dark",
+}
+
 const App = () => {
   const { isShowing } = useSelector((state) => state.sidebar)
 
   return (
     <Router>
+      <ToastContainer toastOptions={toastOptions} />
       <AuthWrapper>
         <Routes>
           <Route path="/" element={<SharedLayout />}>
@@ -18,6 +45,14 @@ const App = () => {
             <Route path="team" element={<Team />} />
             <Route path="payment" element={<Payment />} />
             <Route path="purchase" element={<Purchase />} />
+            <Route
+              path="dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
           </Route>
         </Routes>
       </AuthWrapper>
