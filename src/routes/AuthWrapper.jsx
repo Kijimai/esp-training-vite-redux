3e0d49f7f791ae8f7a251453755286432da1abf3
John Deen Bucog <JDBucog@yahoo.com>
2022-12-06 00:26:52 -0800

    init AuthWrapper component, import react-loading into AuthWrapper

---
+++ src/routes/AuthWrapper.jsx
@@ -0,0 +1,30 @@
+import { useAuth0 } from "@auth0/auth0-react"
+import Loading from "../components/Loading"
+import styled from "styled-components"
+
+const AuthWrapper = ({ children }) => {
+  const { isLoading, error } = useAuth0()
+
+  if (isLoading) {
+    return <Loading type="cylon" />
+  }
+
+  if (error) {
+    return (
+      <Wrapper>
+        <h2>Something went wrong...</h2>
+        <h3>{error.message}</h3>
+      </Wrapper>
+    )
+  }
+
+  return <>{children}</>
+}
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+`
+
+export default AuthWrapper
