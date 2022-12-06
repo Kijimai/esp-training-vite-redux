08207e836fd848cb7d0ab0d85956243b75b34875
John Deen Bucog <JDBucog@yahoo.com>
2022-12-06 15:43:40 -0800

    import Auth0Provider into main.js, wrap App in Auth0Provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,24 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
-import { Provider } from "react-redux"
+import { Provider as ReduxProvider } from "react-redux"
 import { store } from "./redux/store"
+import { UserProvider } from "./context/UserProvider"
+import { Auth0Provider } from "@auth0/auth0-react"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH_CLIENT_ID}
+      redirectUri={window.location.origin}
+      cacheLocation={"localstorage"}
+    >
+      <UserProvider>
+        <ReduxProvider store={store}>
+          <App />
+        </ReduxProvider>
+      </UserProvider>
+    </Auth0Provider>
   </React.StrictMode>
 )
