f02ac3a1cf7da8125aa339a88638f2b0fd149e56
John Deen Bucog <JDBucog@yahoo.com>
2022-12-06 15:39:14 -0800

    add userContext passing down user and useAuth0 controls

---
+++ src/context/UserProvider.jsx
@@ -0,0 +1,27 @@
+import { useContext, createContext, useState, useEffect } from "react"
+import { useAuth0 } from "@auth0/auth0-react"
+
+const UserContext = createContext()
+
+const UserProvider = ({ children }) => {
+  const { loginWithRedirect, loginWithPopup, logout, user } = useAuth0()
+  const [currentUser, setCurrentUser] = useState(null)
+  useEffect(() => {
+    console.log(user)
+    setCurrentUser(user)
+  }, [user])
+
+  return (
+    <UserContext.Provider
+      value={{ loginWithRedirect, loginWithPopup, logout, user, currentUser }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+const useUserContext = () => {
+  return useContext(UserContext)
+}
+
+export { UserProvider, useUserContext }
