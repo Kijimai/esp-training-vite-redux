45944fa0080f1dd6a4579992d369cdd195ad7ee6
John Deen Bucog <JDBucog@yahoo.com>
2022-12-06 01:12:01 -0800

    init PrivateRoute component

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { useAuth0 } from "@auth0/auth0-react"
+import { Navigate } from "react-router-dom"
 
-const PrivateRoute = () => {
-  return (
-    <div>PrivateRoute</div>
-  )
+const PrivateRoute = ({ children }) => {
+  const { user } = useAuth0()
+
+  if (!user) {
+    return <Navigate to="/" />
+  }
+
+  return children
 }
 
 export default PrivateRoute
