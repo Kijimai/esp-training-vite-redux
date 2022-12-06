b8253d12df7cd9fe9a625d8c7d199fd7bb258f40
John Deen Bucog <JDBucog@yahoo.com>
2022-12-06 01:12:25 -0800

    add AuthWrapper in App.js wrapping around routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,23 @@ import SharedLayout from "./components/SharedLayout"
 import { Main, Payment, Login, Purchase, Services, Team } from "./routes/index"
 import Sidebar from "./components/navigator/Sidebar"
 import { useSelector } from "react-redux"
-
+import AuthWrapper from "./routes/AuthWrapper"
 const App = () => {
   const { isShowing } = useSelector((state) => state.sidebar)
 
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Main />} />
-          <Route path="services" element={<Services />} />
-          <Route path="team" element={<Team />} />
-          <Route path="payment" element={<Payment />} />
-          <Route path="purchase" element={<Purchase />} />
-        </Route>
-      </Routes>
+      <AuthWrapper>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Main />} />
+            <Route path="services" element={<Services />} />
+            <Route path="team" element={<Team />} />
+            <Route path="payment" element={<Payment />} />
+            <Route path="purchase" element={<Purchase />} />
+          </Route>
+        </Routes>
+      </AuthWrapper>
       {isShowing && <Sidebar />}
     </Router>
   )
