d989c2cad16a0a1a907fb066a8efd0772b0cdd60
John Deen Bucog <JDBucog@yahoo.com>
2022-12-06 16:37:51 -0800

    add toast notification on unauthorized access in PrivateRoute

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,10 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react"
 import { Navigate } from "react-router-dom"
+import { toast } from "react-toastify"
 
 const PrivateRoute = ({ children }) => {
   const { user } = useAuth0()
 
   if (!user) {
+    toast.warn("You do not have permission to view this page! Please login or register to view the dashboard.")
+    
     return <Navigate to="/" />
   }
 
