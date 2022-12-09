f8127493d66cd0545470ee7af5bfc8110e8aeb2c
John Deen Bucog <JDBucog@yahoo.com>
2022-12-09 15:42:35 -0800

    init createPaymentIntent function, add stripe states for conditional rendering and functionality in form, init CheckOutForm with styling

--- src/components/StripeCheckout.jsx
+++ src/components/StripeCheckout.jsx
@@ -4,36 +4,92 @@ import { loadStripe } from "@stripe/stripe-js"
 import {
   Elements,
   useElements,
+  PaymentElement,
   CardElement,
   CardCvcElement,
   CardExpiryElement,
   useStripe,
 } from "@stripe/react-stripe-js"
 import { useAuth0 } from "@auth0/auth0-react"
+import { useParams } from "react-router-dom"
+import Error from "../routes/Error"
+import { purchaseTiers } from "../links"
 const promise = loadStripe(import.meta.env.VITE_STRIPE_KEY)
 
 const CheckOutForm = () => {
   const { user } = useAuth0()
   const stripe = useStripe()
   const elements = useElements()
-  
-  const createPaymentIntent = async () => {}
+  const { plan } = useParams()
 
-  console.log(user)
+  // Stripe States
+  const [succeeded, setSucceeded] = useState(false)
+  const [processing, setProcessing] = useState(false)
+  const [disabled, setDisabled] = useState(true)
+  const [clientSecret, setClientSecret] = useState("")
+  const [error, setError] = useState(null)
 
-  return <div>Hello Diveasdasdasdasda</div>
+  const createPaymentIntent = async () => {
+    try {
+    } catch (err) {
+      console.log(err.response)
+    }
+  }
+  console.log(plan)
+
+  useEffect(() => {
+    setClientSecret()
+  }, [])
+
+  if (
+    plan !== "basic" &&
+    plan !== "pro" &&
+    plan !== "deluxe" &&
+    plan !== "mvp"
+  ) {
+    return <Error />
+  }
+
+  const { nickname } = user
+
+  // Get current plan based on route
+  const currentPlan = purchaseTiers.find((tier) => tier.userType === plan)
+
+  return (
+    <PlanWrapper>
+      <div className="left">
+        <h2>{nickname}</h2>
+        <ul>
+          {currentPlan.offers.map((offer, idx) => {
+            return <li key={idx}>{offer}</li>
+          })}
+        </ul>
+      </div>
+      <div className="right">
+        <form>
+          <CardElement />
+        </form>
+      </div>
+    </PlanWrapper>
+  )
 }
 
 const StripeCheckout = () => {
   return (
-    <Wrapper>
+    <StripeWrapper>
       <Elements stripe={promise}>
         <CheckOutForm />
       </Elements>
-    </Wrapper>
+    </StripeWrapper>
   )
 }
 
-const Wrapper = styled.aside``
+const PlanWrapper = styled.main`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  background-color: hsl(var(--clr-white));
+`
+
+const StripeWrapper = styled.aside``
 
 export default StripeCheckout
