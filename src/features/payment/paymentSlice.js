6e37b998c49d62b023a68b62662d55a5298307fb
John Deen Bucog <JDBucog@yahoo.com>
2022-12-09 14:05:53 -0800

    add paymentSlice for use in redux, updaate basic, pro, deluxe and mvp plan costs in reducer, export reducers

--- src/features/payment/paymentSlice.js
+++ src/features/payment/paymentSlice.js
@@ -1,2 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"
 
+const initialState = {
+  planCost: 0,
+}
+
+const paymentSlice = createSlice({
+  name: "payment",
+  initialState,
+  reducers: {
+    updateBasicPlan: (state) => {
+      state.planCost = 2500
+    },
+    updateProPlan: (state) => {
+      state.planCost = 5000
+    },
+    updateDeluxePlan: (state) => {
+      state.planCost = 22500
+    },
+    updateMVPPlan: (state) => {
+      state.planCost = 100000
+    },
+  },
+})
+
+export const {
+  updateBasicPlan,
+  updateDeluxePlan,
+  updateMVPPlan,
+  updateProPlan,
+} = paymentSlice.actions
+
+export default paymentSlice.reducer
