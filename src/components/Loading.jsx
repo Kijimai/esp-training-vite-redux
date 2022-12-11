164230cd59b52a13f2671ec6b0376dddafe3bf35
John Deen Bucog <JDBucog@yahoo.com>
2022-12-10 16:47:47 -0800

    add default values for Loading Component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,9 @@
 import ReactLoading from "react-loading"
 
-const Loading = ({ type, color = "#00cf80" }) => {
-  return <ReactLoading type={type} color={color} height={400} width={400} />
+const Loading = ({ type, color = "#00cf80", height = 400, width = 400 }) => {
+  return (
+    <ReactLoading type={type} color={color} height={height} width={width} />
+  )
 }
 
 export default Loading
