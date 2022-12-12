d85e6beb06484e09df89f88d2fc6766f29a6243d
John Deen Bucog <JDBucog@yahoo.com>
2022-12-11 18:19:29 -0800

    add email for email_receipt property on function call -- on a live payment service, this automatically sends an email invoice to the user's email

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -4,14 +4,13 @@ const stripe = require("stripe")(process.env.VITE_STRIPE_SECRET_KEY)
 
 exports.handler = async function (event, context) {
   if (event.body) {
-    const { cost } = JSON.parse(event.body)
-
+    const { cost, email } = JSON.parse(event.body)
     try {
       const paymentIntent = await stripe.paymentIntents.create({
         amount: cost,
+        receipt_email: email,
         currency: "usd",
       })
-      console.log("payment intent", paymentIntent)
 
       return {
         statusCode: 200,
